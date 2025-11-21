2cfa1d958313bb87681aee235707abe8641d7282
miloschwartz <mschwartz10612@gmail.com>
2025-11-21 18:13:39 -0500

    change msi output file name

--- build-msi.bat
+++ build-msi.bat
@@ -2,4 +2,4 @@
 REM Build MSI installer for Pangolin
 REM This script creates the MSI installer from an already-built executable
 
-wix.exe build -arch x64 -define BuildDir=build -define ProjectDir=. -o build\Pangolin.msi installer\pangolin.wxs
+wix.exe build -arch x64 -define BuildDir=build -define ProjectDir=. -o build\Pangolin_Installer.msi installer\pangolin.wxs
